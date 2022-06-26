86b1496f5b65470fe7e0e539576b9d3ec10f2801
Walter Cabral <walterpylson@live.com.pt>
2022-06-26 20:32:07 +0100

    new list app

---
+++ lib/list_app/screens/list_app_main_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListaAppMainScreen extends StatefulWidget {
+  ListaAppMainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ListaAppMainScreen> createState() => _ListaAppMainScreenState();
+}
+
+class _ListaAppMainScreenState extends State<ListaAppMainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
